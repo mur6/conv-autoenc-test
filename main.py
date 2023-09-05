f044fa80584480f03d631b52c8cd20c257b4fc9c
MURAKI Taichi <muraki.taichi@gmail.com>
2023-09-05 16:43:58 +0900

    fix

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import torch
+from torch import nn
+from torch.utils.data import DataLoader
+import torchvision
+from torchvision import transforms
+from torchvision.datasets import CIFAR10
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def imshow(img):
+    img = torchvision.utils.make_grid(img)
+    img = img / 2 + 0.5
+    npimg = img.detach().numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
