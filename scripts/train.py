2d9f79bed332eee55ec8f5947df7036c51e2f891
MURAKI Taichi <muraki.taichi@gmail.com>
2023-09-06 18:39:33 +0900

    fix

--- scripts/train.py
+++ scripts/train.py
@@ -61,34 +61,52 @@ class Autoencoder(nn.Module):
         # Encoder layers
         self.encoder = nn.Sequential(
             # conv1
-            nn.Conv2d(1, 32, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=2),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             # conv2
-            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=2),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             # conv3
-            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=2),
             nn.BatchNorm2d(128),
             nn.ReLU(),
         )
 
         # Decoder layers
+        # self.decoder = nn.Sequential(
+        #     nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1),
+        #     nn.ReLU(),
+        #     # nn.Upsample(scale_factor=2, mode='bilinear'),
+        #     nn.ConvTranspose2d(
+        #         64, 32, kernel_size=3, stride=2),
+        #     nn.ReLU(),
+        #     nn.Upsample(scale_factor=2, mode='bilinear'),
+        #     # nn.ConvTranspose2d(
+        #     #     32, 1, kernel_size=3, stride=2
+        #     # ),
+        #     nn.Sigmoid(),  # Output values between 0 and 1 for binary images
+        # )
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1),
+            # conv 4
+            nn.ConvTranspose2d(in_channels=128, out_channels=64,
+                               kernel_size=3, stride=1, padding=2),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
-            # nn.Upsample(scale_factor=2, mode='bilinear'),
-            nn.ConvTranspose2d(
-                64, 32, kernel_size=3, stride=2),
-            nn.ReLU(),
+            # conv 5
             nn.Upsample(scale_factor=2, mode='bilinear'),
-            # nn.ConvTranspose2d(
-            #     32, 1, kernel_size=3, stride=2
-            # ),
-            nn.Sigmoid(),  # Output values between 0 and 1 for binary images
+            nn.ConvTranspose2d(in_channels=64, out_channels=32,
+                               kernel_size=3, stride=1, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            # conv 6 out
+            nn.Upsample(scale_factor=2, mode='bilinear'),
+            nn.ConvTranspose2d(in_channels=32, out_channels=1,
+                               kernel_size=3, stride=1, padding=2),
+            nn.Softmax()
         )
 
     def forward(self, x):
