445c216bbf1fee1ca40674bade7735d64e3ce316
MURAKI Taichi <muraki.taichi@gmail.com>
2023-09-06 15:44:02 +0900

    fix

--- data_aug_test.py
+++ data_aug_test.py
@@ -27,22 +27,19 @@ class MaskDataset(Dataset):
     def __init__(self, transform=None):
         # self.images_filepaths = list(Path(images_folder).glob("*.jpeg"))
         p = Path("data/simple-rectangle") / "argumented_masks.pt"
-        self.masks = torch.load(p)
+        self.masks = torch.load(p).numpy()
         self.transform = transform
 
     def __len__(self):
-        return len(self.images_filepaths)
+        return self.masks.shape[0]
 
     def __getitem__(self, idx):
-        image_filepath = self.images_filepaths[idx]
-        pil_image = Image.open(image_filepath)
-        image = np.asarray(pil_image)
-
+        image = self.masks[idx]
+        # pil_image = Image.open(image_filepath)
+        # image = image.numpy()
         if self.transform is not None:
             image = self.transform(image=image)["image"]
-        return image
-
-
+        return image.squeeze(0)
 
 
 # # データ拡張と正規化を組み合わせる
@@ -59,7 +56,7 @@ def main():
     train_transform = A.Compose(
         [
             A.Cutout(num_holes=8, max_h_size=32, max_w_size=32, fill_value=0, p=0.75),
-            A.CoarseDropout(num_holes=24, max_height=16, max_width=16, fill_value=0, p=0.75),
+            A.CoarseDropout(max_holes=64, max_height=16, max_width=16, fill_value=0, p=0.75),
             # A.(),
             # .Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             ToTensorV2(),
