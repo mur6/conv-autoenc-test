d365f61cde0ef65e7f7cc826d0ff5265e3464252
MURAKI Taichi <muraki.taichi@gmail.com>
2023-09-06 14:48:58 +0900

    fix

--- dataset_test.py
+++ dataset_test.py
@@ -13,7 +13,7 @@ from PIL import Image
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 # from torchvision import transforms
-# import torchvision.transforms as T
+import torchvision.transforms as T
 from torchvision.transforms import v2
 
 import torchvision.transforms.v2 as transforms
@@ -27,17 +27,17 @@ transform = transforms.Compose(
 )
 
 def main():
-    masks_np = torch.load("data/simple-rectangle/MASKS_TENSOR.pt").numpy()
+    masks = torch.load("data/simple-rectangle/MASKS_TENSOR.pt") # .numpy()
     # masks_train_loader = DataLoader(masks, batch_size=batch_size, shuffle=True)
     # for m in masks_train_loader:
     #     # print(m[0].shape)
     #     # print(m[0])
     #     im = m[0]
     #     im = transform(im.unsqueeze(0))
-    #     plt.imshow(im.squeeze(0))
-    #     break
-    # plt.show()
-    print(masks_np)
+    im = masks[0]
+    # print(masks_np)
+    plt.imshow(im.squeeze(0))
+    plt.show()
 
 
 if __name__ == "__main__":
