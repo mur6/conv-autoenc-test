1758473feab740d7cb84cbdb8547acb469d175ff
MURAKI Taichi <muraki.taichi@gmail.com>
2023-09-06 13:20:43 +0900

    fix

--- torchvision_image_test.py
+++ torchvision_image_test.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 from PIL import Image
 import albumentations as A
-
+from albumentations.pytorch import ToTensorV2
 # import torchvision.transforms as T
 from torchvision.transforms import v2
 
@@ -69,8 +69,8 @@ albumentations_transform = A.Compose(
 
 def get_images():
     sample_image_path = "../poetry-test-proj/samples/02"
-    images = 
-    print(images)
+    # images = 
+    # print(images)
     images = [Image.open(image) for image in images]
     transform = v2.ToTensor()
     images = list(map(transform, images))
@@ -112,8 +112,21 @@ def main():
     #     plt.imshow(im.squeeze(0))
     #     break
     # plt.show()
-    images = get_images()
+    # images = get_images()
     # print(images)
+    train_transform = A.Compose(
+        [
+            A.SmallestMaxSize(max_size=160),
+            A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=15, p=0.5),
+            A.RandomCrop(height=128, width=128),
+            A.RGBShift(r_shift_limit=15, g_shift_limit=15, b_shift_limit=15, p=0.5),
+            A.RandomBrightnessContrast(p=0.5),
+            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            ToTensorV2(),
+        ]
+    )
+    dataset = ImageDataset(
+        "../poetry-test-proj/samples/02")
     batch_size = 32
     dataloader = DataLoader(
         images,
