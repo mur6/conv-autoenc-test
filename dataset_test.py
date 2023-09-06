9ec65563c3ab79c979c39f6e7ea32e2ae3a4a990
MURAKI Taichi <muraki.taichi@gmail.com>
2023-09-06 15:09:38 +0900

    fix

--- dataset_test.py
+++ dataset_test.py
@@ -19,16 +19,21 @@ from torchvision.transforms import v2
 
 import torchvision.transforms.v2 as transforms
 
-transform = transforms.Compose(
+# transform = transforms.Compose(
+#     [
+#         transforms.ColorJitter(contrast=0.5),
+#         transforms.RandomRotation(30),
+#         transforms.CenterCrop(480),
+#     ]
+# )
+transforms = v2.Compose(
     [
-        transforms.ColorJitter(contrast=0.5),
-        transforms.RandomRotation(30),
-        transforms.CenterCrop(480),
+        v2.RandomZoomOut(p=1.0),
+        v2.Resize((96, 96))
     ]
 )
 
-def main():
-    masks = torch.load("data/simple-rectangle/MASKS_TENSOR.pt") # .numpy()
+def test():
     # masks_train_loader = DataLoader(masks, batch_size=batch_size, shuffle=True)
     # for m in masks_train_loader:
     #     # print(m[0].shape)
@@ -41,13 +46,26 @@ def main():
     print(num, index)
     orig_img = masks[index].unsqueeze(0)
     # print(im.shape)
-    zoomout = v2.RandomZoomOut(p=1.0)
-    images = [zoomout(orig_img) for _ in range(4)]
-    fig, axes = plt.subplots(2, 2, figsize=(9, 9), tight_layout=True)
-    for ax, img in zip(axes.flatten(), images):
-        # ax.imshow(img.permute(1, 2, 0))
-        ax.imshow(img.squeeze(0))
-    plt.show()
+
+
+def augments_data(orig_img):
+    return [zoomout(orig_img.unsqueeze(0)) for _ in range(4)]
+
+
+def main():
+    masks = torch.load("data/simple-rectangle/MASKS_TENSOR.pt")
+    def _iter():
+        for orig_img in masks:
+            yield orig_img
+            yield from augments_data(orig_img)
+    images = list(_iter())
+    images = torch.stack(images)
+    print(images[0].shape)
+    # fig, axes = plt.subplots(2, 2, figsize=(9, 9), tight_layout=True)
+    # for ax, img in zip(axes.flatten(), images):
+    #     # ax.imshow(img.permute(1, 2, 0))
+    #     ax.imshow(img.squeeze(0))
+    # plt.show()
 
 
 if __name__ == "__main__":
