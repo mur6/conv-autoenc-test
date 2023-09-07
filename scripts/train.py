9bc3f34cb343236c86b0b2dd3ffbe1802a35435e
MURAKI Taichi <muraki.taichi@gmail.com>
2023-09-07 09:18:12 +0900

    リファクタリング.

--- scripts/train.py
+++ scripts/train.py
@@ -1,8 +1,6 @@
 from pathlib import Path
-import random
 
 import torch
-from torch import nn
 from torch.utils.data import DataLoader
 import torchvision
 
@@ -15,8 +13,9 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 import torchvision.transforms as T
 from torchvision.transforms import v2
-
 import torchvision.transforms.v2 as transforms
+
+from src.models import Autoencoder
 
 
 def imshow(img):
@@ -51,67 +50,6 @@ def train(net, criterion, optimizer, epochs, trainloader):
         output_and_label.append((output, img))
     print("finished")
     return net, output_and_label, losses
-
-
-# Define the Convolutional Autoencoder architecture
-class Autoencoder(nn.Module):
-    def __init__(self):
-        super(Autoencoder, self).__init__()
-
-        # Encoder layers
-        self.encoder = nn.Sequential(
-            # conv1
-            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=2),
-            # nn.BatchNorm2d(32),
-            nn.ReLU(),
-            # nn.MaxPool2d(kernel_size=2, stride=2),
-            # conv2
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=2),
-            # nn.BatchNorm2d(64),
-            nn.ReLU(),
-            # nn.MaxPool2d(kernel_size=2, stride=2),
-            # conv3
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=2),
-            # nn.BatchNorm2d(128),
-            nn.ReLU(),
-        )
-
-        # Decoder layers
-        # self.decoder = nn.Sequential(
-        #     nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1),
-        #     nn.ReLU(),
-        #     # nn.Upsample(scale_factor=2, mode='bilinear'),
-        #     nn.ConvTranspose2d(
-        #         64, 32, kernel_size=3, stride=2),
-        #     nn.ReLU(),
-        #     nn.Upsample(scale_factor=2, mode='bilinear'),
-        #     # nn.ConvTranspose2d(
-        #     #     32, 1, kernel_size=3, stride=2
-        #     # ),
-        #     nn.Sigmoid(),  # Output values between 0 and 1 for binary images
-        # )
-        self.decoder = nn.Sequential(
-            # conv 4
-            nn.ConvTranspose2d(in_channels=128, out_channels=64,
-                               kernel_size=3, stride=1, padding=2),
-            nn.ReLU(),
-            # conv 5
-            # nn.Upsample(scale_factor=2, mode='bilinear'),
-            nn.ConvTranspose2d(in_channels=64, out_channels=32,
-                               kernel_size=3, stride=1, padding=2),
-            nn.ReLU(),
-            # conv 6 out
-            # nn.Upsample(scale_factor=2, mode='bilinear'),
-            nn.ConvTranspose2d(in_channels=32, out_channels=1,
-                               kernel_size=3, stride=1, padding=2),
-            nn.Softmax()
-        )
-
-    def forward(self, x):
-        x = self.encoder(x)
-        # print(f"x={x.shape}")
-        x = self.decoder(x)
-        return x
 
 
 class MaskDataset(Dataset):
@@ -185,7 +123,6 @@ def main():
         # print(f"epoch: {epoch}, ", end="")
         total_loss = 0
         for img, label_img in train_loader:
-
             img = img.to(device)
             label_img = label_img.to(device)
             # print(f"img={img.shape} {img.dtype}")
