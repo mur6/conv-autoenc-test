87719799b40cab38791e47453a6ce02abecb83d9
MURAKI Taichi <muraki.taichi@gmail.com>
2023-09-07 15:46:11 +0900

    test

--- ae_test.py
+++ ae_test.py
@@ -5,26 +5,24 @@ import torch.nn.functional as F
 from src.models import CVAEv2
 
 
-class AutoEncoderV6(nn.Module):
-    def __init__(self):
+
+class CVAEv3(nn.Module):
+    def __init__(self, device):
         super().__init__()
-        latent_dim = 8
+        self.device = device
+        latent_dim = 16
         self.encoder = nn.Sequential(
             nn.Conv2d(1, 16, kernel_size=4, padding=1, stride=2),
             nn.ReLU(),
             nn.Conv2d(16, 32, kernel_size=4, padding=1, stride=2),
             nn.ReLU(),
-            # nn.Conv2d(32, 64, kernel_size=4, padding=1, stride=2),
-            # nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(24 * 24 * 32, latent_dim * 2),
+            nn.Linear(64 * 12 * 12, latent_dim * 2),
         )
         self.decoder = nn.Sequential(
-            nn.Linear(latent_dim, 24 * 24 * 32),
+            nn.Linear(latent_dim, 12 * 12 * 64),
             nn.ReLU(),
-            nn.Unflatten(1, (32, 24, 24)),
-            # nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
-            # nn.ReLU(),
+            nn.Unflatten(1, (64, 12, 12)),
             nn.ConvTranspose2d(32, 16, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
             nn.ConvTranspose2d(16, 1, kernel_size=4, stride=2, padding=1),
@@ -32,23 +30,29 @@ class AutoEncoderV6(nn.Module):
         )
 
     def reparametrizaion(self, mean, log_var, device):
-        eps = torch.randn(mean.shape).to(device)
-        return mean + torch.sqrt(log_var) * eps
+        eps = torch.randn_like(mean).to(device)
+        return mean + torch.exp(log_var / 2) * eps
 
-    def forward(self, x, device):
+    def forward(self, x):
         # x = x.view(-1, self.x_dim)
         x = self.encoder(x)
         mean, log_var = torch.chunk(x, 2, dim=1)
+        # print("mean=", mean)
+        # print("log_var=", log_var)
+        # log_var = F.softplus(log_var)
+        # print("log_var=", log_var)
         # print(f"mean={mean.shape} log_var={log_var.shape}")
-        z = self.reparametrizaion(mean, log_var, device)
-        x_hat = self.decoder(z)  # 潜在ベクトルを入力して、再構築画像 y を出力
+        z = self.reparametrizaion(mean, log_var, self.device)
+        # z = F.relu(z)
+        # print("z=", z)
+        x_hat = self.decoder(z)
         return x_hat, z, mean, log_var
 
 
 def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    x = torch.rand(8, 1, 96, 96)
-    model = CVAEv2(device)
+    x = torch.rand(8, 1, 32, 32)
+    model = CVAEv3(device)
     # enc = model.encoder
     output, z, ave, log_dev = model(x)
     print(f"output={output.shape} z={z.shape}")
