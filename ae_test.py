af96add55da64eb60240c84869ed600c3e001c8d
MURAKI Taichi <muraki.taichi@gmail.com>
2023-09-07 09:49:46 +0900

    fix

--- ae_test.py
+++ ae_test.py
@@ -5,14 +5,23 @@ from torch import nn
 class AutoEncoderV0(nn.Module):
     def __init__(self):
         super().__init__()
+        latent_dim = 8
         self.enc = nn.Sequential(
             nn.Conv2d(1, 16, kernel_size=4, padding=1, stride=2),
             nn.ReLU(),
-            # nn.MaxPool2d(2),
             nn.Conv2d(16, 32, kernel_size=4, padding=1, stride=2),
             nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, padding=1, stride=2),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(12 * 12 * 64, latent_dim)
         )
         self.dec = nn.Sequential(
+            nn.Linear(latent_dim, 12 * 12 * 64),
+            nn.ReLU(),
+            nn.Unflatten(1, (64, 12, 12)),
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
             nn.ConvTranspose2d(32, 16, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
             nn.ConvTranspose2d(16, 1, kernel_size=4, stride=2, padding=1),
@@ -28,6 +37,7 @@ class AutoEncoderV0(nn.Module):
 def main():
     x = torch.rand(8, 1, 96, 96)
     model = AutoEncoderV0()
+    enc = model.enc
     out = model(x)
     print(f"out={out.shape}")
 
